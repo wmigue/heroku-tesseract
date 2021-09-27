93825868ac29ae1d8ba3d1fcb5af1aa0d46cdd60
unknown <wmigue@gmail.com>
2021-09-27 11:45:11 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useState} from 'react';
+import Tesseract from 'tesseract.js';
 import './App.css';
-
+ 
 function App() {
+  const [imagePath, setImagePath] = useState("");
+  const [text, setText] = useState("");
+ 
+  const handleChange = (event) => {
+    setImagePath(URL.createObjectURL(event.target.files[0]));
+  }
+ 
+  const handleClick = () => {
+  
+    Tesseract.recognize(
+      imagePath,'eng',
+      { 
+        logger: m => console.log(m) 
+      }
+    )
+    .catch (err => {
+      console.error(err);
+    })
+    .then(result => {
+      // Get Confidence score
+
+      //let confidence = r.confidence
+     
+      let text = result.data.text
+      console.log(text)
+      setText(result.data.text);
+  
+    })
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-main">
+        <h3>Actual imagePath uploaded</h3>
+        <img 
+           src={imagePath} className="App-image" alt="logo"/>
+        
+          <h3>Extracted text</h3>
+        <div className="text-box">
+          <p> {text} </p>
+        </div>
+        <input type="file" onChange={handleChange} />
+        <button onClick={handleClick} style={{height:50}}> convert to text</button>
+      </main>
     </div>
   );
 }
-
-export default App;
+ 
+export default App
